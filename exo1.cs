ea53d9a2dae0e0900dc6499758948a0072a7c762
Simon Martinenq <simon.martinenq@efrei.net>
2022-09-29 16:33:36 +0200

    Update exo1

--- exo1.cs
+++ exo1.cs
@@ -1,56 +1,3 @@
-/* // Programmation parallèle
-
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-
-class Program {
-	
-	static void Factoriel(int n){
-	    Console.WriteLine("sequentielle");
-
-	    int i = 1;
-		while(n!=1){
-			i = i*n;
-			n=n-1;
-		}
-	    
-	    Thread.Sleep(1000);
-	    Console.WriteLine(i);
-	}
-	
-    public static async Task<int> Factorial(int n){
-        Console.WriteLine("Factorial");
-        int i = 1;
-      
-		while(n!=1){
-			i = i*n;
-			n=n-1;
-		}
-		Thread.Sleep(3000);
-	    return i;
-    }
-	
-	static async void FactorialAsync(int n){
-	    Console.WriteLine("async");
-	    Task<int> task = Factorial(n);
-	    Console.WriteLine("avant");
-	    Thread.Sleep(4000);
-	    Console.WriteLine("apres");
-        int count = await task;
-        Console.WriteLine(count);
-
-	}
-
-    static void Main(string[] args) {
-        FactorialAsync(3);
-		//Factoriel(3);
-		
-    }
-} */
-
-// Programmation parallèle
-
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -58,8 +5,6 @@ using System.Threading.Tasks;
 class Program {
 	
 	static void Factorial(int n){
-	    Console.WriteLine("sequentielle");
-
 	    int i = 1;
 		while(n!=1){
 			i = i*n;
@@ -67,22 +12,21 @@ class Program {
 		}
 	    
 	    Thread.Sleep(3000);
+	    Console.WriteLine("Sync way");
 	    Console.WriteLine(i);
 	}
 	
 
 	
 	static async void FactorialAsync(int n){
-	    Console.WriteLine("Async");
+	    Console.WriteLine("Async way");
 	    await Task.Run(()=>Factorial(n));
 	}
 	
 
     static void Main(string[] args) {
         FactorialAsync(3);
-        Console.Read();
-		//Factorial(3);
-	
-		
+		Factorial(3);
+		Console.Read();
     }
 }
